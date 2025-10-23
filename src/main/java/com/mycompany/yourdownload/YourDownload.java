873ca7a20a0567bbab05e523e32ec134f3966fa8
Rubén Gómez <rubengomez@paucasesnovescifp.cat>
2025-10-23 20:01:40 +0200

    Refactor: renamed project and main class

---
+++ src/main/java/com/mycompany/yourdownload/YourDownload.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.YourDownload;
+
+/**
+ *
+ * @author LionKeriot
+ */
+public class YourDownload {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("Probando github");
+    }
+}
