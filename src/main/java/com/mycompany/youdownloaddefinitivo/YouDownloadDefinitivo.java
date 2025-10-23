162ef268b1f5fe554dba8eed57ec15fb3292c98e
Rubén Gómez <rubengomez@paucasesnovescifp.cat>
2025-10-23 17:49:54 +0200

    Proyecto nuevo creado desde cero en NetBeans 27

---
+++ src/main/java/com/mycompany/youdownloaddefinitivo/YouDownloadDefinitivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.youdownloaddefinitivo;
+
+/**
+ *
+ * @author LionKeriot
+ */
+public class YouDownloadDefinitivo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        System.out.println("Probando github");
+    }
+}
